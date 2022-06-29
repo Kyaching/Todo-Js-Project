9b0443251ca513d24a21fdc2d531b6859ebbece1
Kyaching <kyochingmarma@gmail.com>
2022-06-29 19:07:52 +0600

    modified todo app functionality

--- app.js
+++ app.js
@@ -1,47 +1,92 @@
-const userText = document.querySelector(".user-text");
+/**
+ * ****************************************
+ *  TODO task app using Vanilla Javascipt *
+ * ****************************************
+ */
+
+// DOM elements selector
+const inputTask = document.querySelector(".user-text");
 const addButton = document.querySelector(".add-button");
-const displayText = document.querySelector(".show-output");
-const deleteButton = document.querySelector(".delete");
-const editButton = document.querySelector(".edit-button");
+const display = document.getElementById("post");
+const popUp = document.querySelector(".popup");
 
-
-// add text
-addButton.addEventListener('click',e =>{
-    e.preventDefault();
-    formValidation();
+// Adding task using button
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  formValidation();
 });
 
-deleteButton.addEventListener('click', () =>{
-    deleteText();
-})
+// Validate user input data
+let formValidation = () => {
+  if (inputTask.value === "") {
+    popUp.style.display = "block";
 
-editButton.addEventListener('click', () =>{
-    userText.contentEditable = true;
-    userText.style.backgroundColor = "#121212";
-})
+    // Warning will pop up
+    popUp.innerHTML = "Task can't be blank!";
 
-let formValidation = () =>{
-    if(userText.value ===''){
-        displayText.innerHTML = "sorry!";
-    }else{
-        acceptData();
-    }
-}
+    // Waring will be automatically popdown after 1 second
+    setTimeout(() => {
+      popUp.style.display = "none";
+    }, 1000);
+  } else {
+    acceptData();
+  }
+};
 
+// array of months
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// Creating Date object
+const d = new Date();
+
+// getting day, month & year object property
+let date = d.getDate();
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+// Creating Empty data Object
 let data = {};
 
-let acceptData = () =>{
-    data ["text"] = userText.value;
-    console.log(data);
-    createPost();
-}
+// Taking data from user
+let acceptData = () => {
+  data["text"] = inputTask.value;
+  createTask();
+};
 
-let createPost = () => {
-    displayText.innerHTML += `<div class="show-output">${data.text}</div>
-    <button class="delete">DEL</button>`;
-    userText.value = '';
-}
+// Show user input in DOM
+let createTask = () => {
+  display.innerHTML += `<div class="posts">
+  <p>${data.text}</p>
+  <sup><span class="calender">${date} ${month} ${year}</span></sup>
+  <span class="options">
+    <i onClick="editTask(this)" class="fas fa-edit"></i>
+    <i onClick="deleteTask(this)" class="fas fa-trash-alt"></i>
+  </span>
+</div>`;
+  inputTask.value = "";
+};
 
-let deleteText = () => {
-    displayText.innerHTML = '';
-}
+// Delete task
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+// Edit task
+let editTask = (e) => {
+  inputTask.value =
+    e.parentElement.previousElementSibling.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
